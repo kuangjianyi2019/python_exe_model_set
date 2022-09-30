e49f1c09936ea25fbeeadcf088878b95ae4e5d2c
邝建亿 <kuangjianyi2019@163.com>
2022-09-30 15:34:25 +0800

    组件属性配置1

--- ini_board/ini_component.py
+++ ini_board/ini_component.py
@@ -15,18 +15,22 @@ from util.util import load_yaml
 
 
 class IniComponent():
-
-    dict_botton :Dict[str, Button]={}
-    dict_radiobutton :Dict[str, Radiobutton]={}
-    dict_frame : Dict[str, Frame]={}
-    dict_labelframe : Dict[str, LabelFrame]={}
+    dict_botton: Dict[str, Button] = {}
+    dict_radiobutton: Dict[str, Radiobutton] = {}
+    dict_frame: Dict[str, Frame] = {}
+    dict_labelframe: Dict[str, LabelFrame] = {}
     dict_label: Dict[str, Label] = {}
     dict_combobox: Dict[str, ttk.Combobox] = {}
-    dict_text : Dict[str, Text]={}
-    dict_GLOBAL_VARIABLE : Dict[str, IntVar]={}
+    dict_text: Dict[str, Text] = {}
+    dict_scrollbar: Dict[str, Scrollbar] = {}
+    dict_GLOBAL_VARIABLE: Dict[str, IntVar] = {}
+    record_has_save_component = set()
 
-    def __init__(self):
-        self.set_component()
+    def __init__(self, tk: tkinter.Tk):
+        self.tk = tk
+        self.component_yaml_config = load_yaml('ini_component')
+        self.save_component_obj()
+        self.set_component_config()
 
     def botton(self):
 
@@ -59,23 +63,61 @@ class IniComponent():
         ini_scrollbar = Scrollbar()
         return ini_scrollbar
 
-    def ini_component(self, method_name, flag=1):
+    def get_obj_property(self, method_name, flag=1):
+        '''反射构建组件对象'''
         if flag == 1:
             if hasattr(self, method_name):
-                func = getattr(self, method_name)
-                return func()
+                obj_func = getattr(self, method_name)
+                return obj_func()
         else:
             if hasattr(self, 'dict_' + method_name):
-                variable = getattr(self, 'dict_' + method_name)
-                return variable
+                obj_variable = getattr(self, 'dict_' + method_name)
+                return obj_variable
 
-    def set_component(self):
-        component_config = load_yaml('ini_component')
-        for key, value in component_config.items():
-            if value[0] != None:
-                component = self.ini_component(key)
-                component_dict = self.ini_component(key, flag=2)
-                component_dict[key]=component
+    def save_component_obj(self):
+        '''保存每一个组件'''
+        for component_type, many_component_config in self.component_yaml_config.items():
+
+            if many_component_config:
+
+                for component_name in many_component_config:
+                    component = self.get_obj_property(component_type)
+                    component_variable = self.get_obj_property(component_type, flag=2)
+                    self.record_has_save_component.add(component_type)
+                    component_variable[component_name] = component
+
+    def set_component_config(self):
+        '''给组件设置属性'''
+        for component_type in self.record_has_save_component:
+            many_component_obj = self.get_obj_property(component_type, flag=2)
+            many_component_config = self.component_yaml_config.get(component_type)
+
+            # 获取已创建的组件dict
+            for component_name, single_component_obj in many_component_obj.items():
+                single_component_config = many_component_config.get(component_name)
+                component_frame_parms = single_component_config.get('frame')
+                component_text_parms = single_component_config.get('text')
+                component_func_parms = single_component_config.get('func')
+                component_pack_parms = single_component_config.get('pack')
+                if component_frame_parms == 0:
+                    single_component_obj.master = self.tk
+                else:
+                    # 获取对应的frame
+                    frame_parms = component_frame_parms.split('-')
+                    swap_many_component_obj = self.get_obj_property(frame_parms[0], flag=2)
+                    single_component_obj.master = swap_many_component_obj.get(frame_parms[1])
+
+                if component_text_parms:
+                    single_component_obj.config(text=component_text_parms)
+
+                #todo 方法传入
+                if component_func_parms:
+                    single_component_obj.config(command=print)
+
+                if component_pack_parms == 0:
+                    single_component_obj.pack()
+
 
 if __name__ == '__main__':
-    ic = IniComponent()
+    ic = IniComponent(Tk())
+    ic.tk.mainloop()
